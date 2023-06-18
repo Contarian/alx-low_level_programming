7e095a5e3e13df304855b410f82790f7d8c0fb51
joel <joelbabs1992@gmail.com>
2023-06-18 01:08:35 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - Prints some letters of alphabet
+  *
+  * Return: Always (1)
+  */
+int main(void)
+{
+     int d = 0;
+
+     for (d = 0; d < 10; d++);
+
+     printf("%i \n", d);
+}	
