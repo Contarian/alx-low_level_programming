076a1e9367ff03c4827ec9248d796ff7e6af3380
joel <joelbabs1992@gmail.com>
2023-06-19 23:49:08 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * command that will print _putchar
+ *
+ * return always 0 if (success)
+ *
+ */
+
+int main (void)
+
+{
+     _putchar("_putchar%c\n", _putchar);
+
+     _putchar('\n');
+
+     return 0;
+}	
