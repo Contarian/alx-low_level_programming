bea15e6aac9866455915c236ec22849fbcc37bbc
joel <joelbabs1992@gmail.com>
2023-06-20 05:02:03 -0700

    alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,14 +10,9 @@ int print_alphabet(void)
 {       
     char c;
 
-    for(c = 'a'; c <= 'a'; c++)
-    
-    {
+    for (c = 'a'; c <= 'a'; c++)
 
     _putchar(c);
-
-    
-    }
     
     _putchar('\n');
 }
