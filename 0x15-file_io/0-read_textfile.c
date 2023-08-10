9b918f777e938cc037ef5124db65c44f3480dd65
joel <joelbabs1992@gmail.com>
2023-08-09 22:40:53 -0700

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,30 +1,47 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - read the  txt file and print it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: num of the  letters it should read and to be  printed
+ * read_textfile - to read the txt file and prints to the POSIX stdout
+ * @filename: name of the file that's read
+ * @letters: the number of letters read and printed
  *
- * Return:the actual num of the letters that  it could read and printed
+ * Return: the actual  numb of the letters to be  read and to be  printed, 0 if the file cannot be
+ * opened or read, 0 if @filename is NULL, 0 if write fails.
+ * the expected amount of bytes
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	char *buff;
+	int fd, rd, wr;
+
+	if (filename == NULL)
+		return (0);
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buff);
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
-
-	free(buf);
+	}
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+	wr = write(STDOUT_FILENO, buff, rd);
+	if (wr == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
 	close(fd);
-	return (w);
+	free(buff);
+	return (wr);
 }
-
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,17 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-int _putchar(char c)
-int _strlen(char *str);
+int _putchar(char c);
 int append_text_to_file(const char *filename, char *text_content);
-
 #endif
